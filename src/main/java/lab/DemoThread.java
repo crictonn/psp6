b76a022053bee1acf519a1f5503f018405bd06d8
crictonn <devicejoy@gmail.com>
2023-11-13 19:06:37 +0300

    Ядро летает, осталось только как-то впихнуть его в ActionListener, чтобы оно начинало лететь по нажатию кнопки

--- src/main/java/lab/DemoThread.java
+++ src/main/java/lab/DemoThread.java
@@ -1,6 +1,8 @@
 package lab;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Random;
@@ -9,6 +11,9 @@ import javax.swing.*;
 public class DemoThread extends JFrame{
     private BufferedImage buffCanon, buffCannonball, buffTarget, buffExplosion;
     private Random randomTarget;
+    private int cannonBallXPosition = 175; //Сделать 200, чтобы была красиво появлялся из пушки
+
+    static JButton shootButton = new JButton("Shoot!");
     public DemoThread(){
         setTitle("I LOVE GUI IN JAVA OMG");
         setSize(new Dimension(500, 500));
@@ -16,12 +21,17 @@ public class DemoThread extends JFrame{
         setVisible(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+
+
         JPanel content = new JPanel(new BorderLayout());
         setContentPane(content);
 
         content.add(new Canon(), BorderLayout.EAST);
         content.add(new Target(), BorderLayout.WEST);
-        content.add(new JButton("Shoot"), BorderLayout.SOUTH);
+        content.add(new CannonBall(), BorderLayout.CENTER);
+        content.add(shootButton, BorderLayout.SOUTH);
+
+        shootButton.addActionListener(new ShotPerformed());
 
 
     }
@@ -45,7 +55,7 @@ public class DemoThread extends JFrame{
         protected void paintComponent(Graphics graphics){
             super.paintComponent(graphics);
             Graphics2D graphics2D = (Graphics2D) graphics;
-            graphics2D.drawImage(buffCanon,25,300,100,100,this);
+            graphics2D.drawImage(buffCanon,0,300,100,100,this);
         }
     }
 
@@ -88,6 +98,45 @@ public class DemoThread extends JFrame{
             }
         }
     }
+
+    public class CannonBall extends JPanel{
+        public CannonBall(){
+            setPreferredSize(new Dimension(500, 500));//Вот здесь оно как будто не нужно, из-за того, что центер автоматически выравнивается по EAST и WEST
+            try{
+                buffCannonball = ImageIO.read(new File("C://Users/devic/IdeaProjects/psp6/src/main/resources/cannonball.png"));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(true){
+                        cannonBallXPosition--;
+                        repaint();
+                        try {
+                            Thread.sleep(1);
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+        @Override
+        protected void paintComponent(Graphics graphics){
+            super.paintComponent(graphics);
+            Graphics2D graphics2D = (Graphics2D) graphics;
+            graphics2D.drawImage(buffCannonball,cannonBallXPosition,315,30,30,this);
+        }
+   }
+
+    public class ShotPerformed implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
 
 
     public static void main(String[] args) {
