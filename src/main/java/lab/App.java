0403776a205b14c850ff787e705ed2548b935e4f
crictonn <devicejoy@gmail.com>
2023-11-13 18:32:40 +0300

    мишень появляется и пропадает на 1-5 секунд

--- src/main/java/lab/App.java
+++ src/main/java/lab/App.java
@@ -1,5 +1,26 @@
 package lab;
 
-public class App {
+import java.awt.*;
+import javax.swing.*;
 
+class App extends JFrame
+{
+    public App()
+    {
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(new JButton("North"), BorderLayout.NORTH);
+        getContentPane().add(new JButton("South"), BorderLayout.SOUTH);
+        getContentPane().add(new JButton("West"), BorderLayout.WEST);
+        getContentPane().add(new JButton("East"), BorderLayout.EAST);
+        getContentPane().add(new JButton("Center"), BorderLayout.CENTER);
+
+        setBounds(100,100,400,300);
+    }
+
+    public static void main(String[] args)
+    {
+        App flt = new App();
+        flt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flt.setVisible(true);
+    }
 }
--- src/main/java/lab/DemoThread.java
+++ src/main/java/lab/DemoThread.java
@@ -19,11 +19,15 @@ public class DemoThread extends JFrame{
         JPanel content = new JPanel(new BorderLayout());
         setContentPane(content);
 
-        content.add(new Canon(), BorderLayout.WEST);
+        content.add(new Canon(), BorderLayout.EAST);
+        content.add(new Target(), BorderLayout.WEST);
+        content.add(new JButton("Shoot"), BorderLayout.SOUTH);
+
+
     }
     private class Canon extends JPanel{
         public Canon(){
-            setPreferredSize(new Dimension(500,500));
+            setPreferredSize(new Dimension(150, 400));
             try{
                 buffCanon = ImageIO.read(new File("C://Users/devic/IdeaProjects/psp6/src/main/resources/canon.png"));
             } catch (IOException e){
@@ -37,14 +41,54 @@ public class DemoThread extends JFrame{
                 }
             }).start();
         }
-
         @Override
         protected void paintComponent(Graphics graphics){
             super.paintComponent(graphics);
             Graphics2D graphics2D = (Graphics2D) graphics;
-            graphics2D.drawImage(buffCanon,350,300,100,100,this);
+            graphics2D.drawImage(buffCanon,25,300,100,100,this);
         }
     }
+
+    public class Target extends JPanel{
+        private boolean status = false;
+        public Target(){
+            setPreferredSize(new Dimension(150, 400));
+            try{
+                buffTarget = ImageIO.read(new File("C://Users/devic/IdeaProjects/psp6/src/main/resources/target.png"));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(true){
+                        repaint();
+                        randomTarget = new Random();
+                        try {
+                            Thread.sleep((randomTarget.nextInt(5 - 1 + 1) + 1) * 1000);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+        @Override
+        protected void paintComponent(Graphics graphics){
+            super.paintComponent(graphics);
+            Graphics2D graphics2D = (Graphics2D) graphics;
+            if(status){
+                graphics2D.drawImage(buffTarget,25,250,0,0,this);
+                status = false;
+            }else{
+                graphics2D.drawImage(buffTarget,25,250,100,100,this);
+                status = true;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         new DemoThread().setVisible(true);
